57a58b2b80808cdadeb0de45d71e23a38a2b1c3e
Kaiby Lima <kaiby.lima@gmail.com>
2022-01-05 09:29:23 -0300

    first commit

---
+++ src/br/com/dio/model/Gato.java
@@ -0,0 +1,37 @@
+package br.com.dio.model;
+
+//import java.util.Objects;
+
+public class Gato {
+	private String nome;
+	private String cor;
+	private int idade;
+	public Gato(String nome, String cor, int idade) {
+		this.setNome(nome);
+		this.setCor(cor);
+		this.setIdade(idade);
+	}
+	public Gato() {
+		// TODO Auto-generated constructor stub
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCor() {
+		return cor;
+	}
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	
+}
